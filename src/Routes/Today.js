ab4e3f915a32a81bac44a07a9920c223e5a681f7
phaizi <faizan.mansur87@gmail.com>
2021-12-19 00:15:08 +0500

    Finish today tab

--- src/Routes/Today.js
+++ src/Routes/Today.js
@@ -1,28 +1,32 @@
 import React, {useContext} from 'react';
 import { useParams } from 'react-router-dom';
 import {  makeStyles } from '@mui/styles';
+import { Box } from '@mui/system';
 import Container from '@mui/material/Container';
 import { WeatherContext } from '../Services/contexts';
 import Loader from '../Components/Loader';
-// import Divider from '@mui/material/Divider';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 
 const useStyles = makeStyles((theme) => ({
     container: {
-        backgroundColor: 'white',
-        padding: '20px',
+        padding: '20px', 
+        alignContent:'center',
+        textAlign:'center',
     },
     weatherContainer:{
+        margin: '0px auto',
         backgroundColor: theme.palette.secondary.light,
-        height:500,
-        margin:'20px 0px',
-        padding:'2px 30px',
+        padding:'20px 30px',
         boxShadow: '5px 5px 25px 2px #888888',
     },
     title: {
         color: theme.palette.secondary.main,
         fontSize: 60,
         fontWeight: 'bold',
-        margin: 0,
+        marginBottom: 20,
+        marginTop: 0,
     },
     cityTitle:{
         color: theme.palette.primary.main,
@@ -44,58 +48,64 @@ const useStyles = makeStyles((theme) => ({
     feelslikespan:{
         color: theme.palette.primary.main,
         fontSize:30,
-        margin:0
+        margin:'20',
     },
-    tagline: {
-        color: theme.palette.primary.main,
-        textAlign: 'center',
-        fontFamily: 'Yusei Magic,sans-serif',
-    },
-    instructions: {
-        color: theme.palette.secondary.main,
-        fontSize: 30,
-        fontWeight: 'bold'
-    },
-    buttonContainer: {
-        display: 'flex', 
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent:'center',
-    }
-
 }))
 
+const selectedKeys = ["pressure",
+"humidity",
+"wind_speed",
+"wind_deg",];
+
+const weatherDetails = ['Pressure', 'Humidity', 'Wind Speed', 'Wind direction']
+
+const units = ['hPa', '%', 'meter/sec', 'degrees']
+
+
 export default function Today(){
+    let weatherValues
     const classes = useStyles();
     const params = useParams();
-    const [weatherData, setWeatheData, isLoading] = useContext(WeatherContext); 
+    const [weatherData, , isLoading] = useContext(WeatherContext); 
     const {timezone, current} = weatherData;
     let dateObj,date
     if(current){
 
         dateObj = new Date(current.dt*1000);
         date = dateObj.toLocaleTimeString("en-US", {timeZone: timezone, month:'long', day:'numeric',year:'numeric', time:'numeric'})
+        weatherValues = selectedKeys.map((item,index)=>(
+            current[item] + ' ' + units[index]
+        ))
     }
-    console.log('this is date = ',date );
+      
     return (
-        <Container className={classes.container}>
-        <h1 className={classes.title}>Today's Weather - <span className={classes.cityTitle}>{params.city}</span></h1>
+        <div className={classes.container}>
+        <h1 className={classes.title}>Today's Weather - <span className={classes.cityTitle}>{params.city.charAt(0).toUpperCase() + params.city.slice(1).toLowerCase()}</span></h1>
+
  <Container className={classes.weatherContainer}>
             {isLoading?<Loader/>:
+ <Box sx={{display:'flex',flexWrap:'wrap',gap: '10px'}}>
             <div>
                 <p className={classes.time}>As on {date}</p>
                 <h1 className={classes.temp}>{current?.temp}°C</h1>
                 <h3 className={classes.feelslike}><span className={classes.feelslikespan}>feels like </span>{current?.feels_like}°C </h3>
                 </div>
-
+<Divider orientation="vertical"  variant="middle" flexItem />
+<List sx={{width:'50%'}} aria-label="weather details">
+<Divider />
+{selectedKeys.map((item,index)=>(
+    <div>
+    <ListItem sx={{ color:'white',fontSize: 20, fontWeight:'medium', height:100 }} secondaryAction={
+                          <h1>{weatherValues && weatherValues[index]}</h1> 
+                  } >
+                      <h1 style={{color:'black'}}>{weatherDetails[index]}</h1>
+    </ListItem>
+    <Divider />
+    </div>
+))}</List>
+</Box>
 }
-
  </Container>
-        {/* <h1 className={classes.tagline}>But Weather app lets you plan your days ahead with more confidence..</h1> */}
-        {/* <Divider variant="middle" /> */}
-        {/* <p className={classes.instructions}>First select the country & then select the city to know the weather there</p> */}
-        {/* <div className={classes.buttonContainer}> */}
-        {/* </div> */}
-    </Container>
+    </div>
     )
 }
