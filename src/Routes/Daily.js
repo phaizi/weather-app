781acbdb4ecea81a882b8de698c1089e19ef7f95
phaizi <faizan.mansur87@gmail.com>
2021-12-19 17:03:58 +0500

    Completed daily tab

--- src/Routes/Daily.js
+++ src/Routes/Daily.js
@@ -6,6 +6,8 @@ import Container from "@mui/material/Container";
 import { WeatherContext } from "../Services/contexts";
 import Loader from "../Components/Loader";
 import NetworkError from "../Components/NetworkError";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -29,13 +31,21 @@ const useStyles = makeStyles((theme) => ({
   cityTitle: {
     color: theme.palette.primary.main,
   },
+  listItemsContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    color: "white",
+  },
 }));
 
 export default function Daily() {
   const classes = useStyles();
   const params = useParams();
-  const [, isLoading, doesNetworkOccured] = useContext(WeatherContext);
-  // const {timezone, dailyData} = weatherData;
+  const [weatherData, , isLoading, doesErrorOccured] =
+    useContext(WeatherContext);
+  const { timezone, daily } = weatherData;
+
 
   return (
     <div className={classes.container}>
@@ -50,10 +60,49 @@ export default function Daily() {
       <Container className={classes.weatherContainer}>
         {isLoading ? (
           <Loader />
-        ) : doesNetworkOccured ? (
+        ) : (doesErrorOccured || !daily) ? (
           <NetworkError />
         ) : (
-          <Box sx={{ display: "flex", flexWrap: "wrap", gap: "10px" }}></Box>
+          <Box sx={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
+            <List sx={{ width: "100%" }} aria-label="weather details">
+              <Divider />
+              {daily?.map((data, index) => {
+                const dateObj = new Date(data.dt * 1000);
+                const date = dateObj.toLocaleDateString("en-US", {
+                  timeZone: timezone,
+                  month: "short",
+                  day: "numeric",
+                  weekday: "short",
+                });
+              
+                return (
+                  <div>
+                    <div className={classes.listItemsContainer}>
+                      <h2 style={{ color: "black", width:'150px' }}>{date}</h2>
+                      <div>
+                      <h1 style={{ width: "150px", marginBottom:0 }}>
+                        {`${(Math.round(data.temp.max * 10) / 10).toFixed(1)}°C/${(Math.round(data.temp.min * 10) / 10).toFixed(1)}°C `}
+                      </h1>
+                      <p style={{ fontSize: 15, margin: 0 }}>
+                          {"Max/Min"}
+                        </p>
+                      </div>
+                      <h2>{data.weather[0].main}</h2>
+                      <div>
+                        <h2 style={{ width: "250px", marginBottom: 0 }}>
+                          {data.wind_speed + " meter/sec"}
+                        </h2>
+                        <p style={{ fontSize: 15, margin: 0 }}>
+                          {"Wind Speed"}
+                        </p>
+                      </div>
+                    </div>
+                    <Divider />
+                  </div>
+                );
+              })}
+            </List>
+          </Box>
         )}
       </Container>
     </div>
